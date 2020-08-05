d05b611eade7b787506b192936844ff1ad14c183
plusmail <plus4957@gmail.com>
2020-08-05 17:07:26 +0900

    111

--- src/test/java/com/yi/coffeemukka/DataSourceTest.java
+++ src/test/java/com/yi/coffeemukka/DataSourceTest.java
@@ -12,8 +12,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/root-context.xml"}) 
-public class DataSourceTest {
-	@Autowired //Spring container 안에 DataSource type을 가진 singleton을 자동 주입시킴
+public class DataSourceTest  {
+	@Autowired //Test	Spring container 안에 DataSource type을 가진 singleton을 자동 주입시킴
 	private DataSource ds;
 	
 	@Test
